489e70ce8d03bd85159e585b8d6f502e8cd287b3
maraarrieche <arrieche.mara@gmail.com>
2023-07-04 20:46:35 +0000

    rest api de starwars terminado

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Usuario, Character, Planeta, Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -36,15 +36,152 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/usuarios', methods=['GET'])
+def get_usuarios():
+    all_usuarios = Usuario.query.all()
+    if all_usuarios is not None:
+        return jsonify([usuario.serialize() for usuario in all_usuarios]), 200
+    else:
+        return jsonify({"message": "Error! Users not found! Try again"}), 404
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/character', methods=['POST'])
+def post_character():
+    body = request.json
 
-    return jsonify(response_body), 200
+    if 'name' not in body:
+        return jsonify({"message": "Error! Asegúrate de enviar 'name' en el body"}), 400
+    if 'description' not in body:
+        return jsonify({"message": "Error! Asegúrate de enviar 'description' en el body"}), 400
+    try:
+        new_character = Character(body['name'], body['description'])
+        db.session.add(new_character)
+        db.session.commit()
+        return jsonify(new_character.serialize()), 200
 
+    except Exception as err:
+        return jsonify({"message": err}), 500
+
+@app.route('/characters', methods=['GET'])
+def get_all_characters():
+    all_characters = Character.query.all()
+    
+    if all_characters is not None:
+        return jsonify([character.serialize() for character in all_characters]), 200
+    else:
+        return jsonify({"message": "Error! Character not found!"}), 404
+
+@app.route('/character/<int:id>', methods=['GET'])
+def get_one_character(id):
+    character = Character.query.get(id)
+
+    if character is not None:
+        return jsonify(character.serialize()), 200
+    else:
+        return jsonify({"message": "Error! Character not found!"}), 404
+
+@app.route('/planeta', methods=['POST'])
+def post_planeta():
+    body = request.json
+
+    if 'name' not in body:
+        return jsonify({"message": "Error! Asegúrate de enviar 'name' en el body"}), 400
+    if 'description' not in body:
+        return jsonify({"message": "Error! Asegúrate de enviar 'description' en el body"}), 400
+    try:
+        new_planeta = Planeta(body['name'], body['description'])
+        db.session.add(new_planeta)
+        db.session.commit()
+        return jsonify(new_planeta.serialize()), 200
+
+    except Exception as err:
+        return jsonify({"message": err}), 500
+
+@app.route('/planetas', methods=['GET'])
+def get_all_planetas():
+    all_planetas = Planeta.query.all()
+    
+    if all_planetas is not None:
+        return jsonify([planeta.serialize() for planeta in all_planetas]), 200
+    else:
+        return jsonify({"message": "Error! Planets not found!"}), 404
+
+@app.route('/planeta/<int:id>', methods=['GET'])
+def get_one_planeta(id):
+    planeta = Planeta.query.get(id)
+
+    if planeta is not None:
+        return jsonify(planeta.serialize()), 200
+    else: 
+        return jsonify({"message": "Error! Planet not found!"}), 404
+
+@app.route('/favorite/planeta/<int:planeta_id>', methods=['POST'])
+def add_planeta_favorite(planeta_id):
+    body = request.json
+    if 'user_id' not in body:
+        return jsonify({"message": "Error! User not found!"}), 400
+    try:
+        new_favorite_planeta = Favorite(None, planeta_id, body['user_id'])
+        db.session.add(new_favorite_planeta)
+        db.session.commit()
+        return jsonify(new_favorite_planeta.serialize()), 200
+
+    except Exception as err:
+        return jsonify({"message": err}), 500
+
+@app.route('/favorite/planeta/<int:planeta_id>', methods=['DELETE'])
+def delete_favorite_planeta(planeta_id):
+    delete_planeta = Favorite.query.filter_by(planeta_id = planeta_id).first()
+    if delete_planeta is not None:
+        db.session.delete(delete_planeta)
+    else:
+        return jsonify({"message": "Error! Planet not found!"}), 404
+    try:
+        db.session.commit()
+        response_body={
+            "done": True
+        }
+        return jsonify(response_body), 200
+    except Exception as err:
+        return jsonify({"message": err}), 500 
+
+@app.route('/favorite/character/<int:character_id>', methods=['POST'])
+def add_character_favorite(character_id):
+    body = request.json
+    if 'user_id' not in body:
+        return jsonify({"message": "Error! User not found!"}), 400
+    try:
+        new_favorite_character = Favorite(character_id, None, body['user_id'])
+        db.session.add(new_favorite_character)
+        db.session.commit()
+        return jsonify(new_favorite_character.serialize()), 200
+
+    except Exception as err:
+        return jsonify({"message": err}), 500
+
+@app.route('/favorite/character/<int:character_id>', methods=['DELETE'])
+def delete_favorite_character(character_id):
+    delete_character = Favorite.query.filter_by(character_id = character_id).first()
+    if delete_character is not None:
+        db.session.delete(delete_character)
+    else:
+        return jsonify({"message": "Error! Character not found!"}), 404
+    try:
+        db.session.commit()
+        response_body={
+            "done": True
+        }
+        return jsonify(response_body), 200
+    except Exception as err:
+        return jsonify({"message": err}), 500 
+
+@app.route('/users/favorites/<int:user_id>', methods=['GET'])
+def get_all_favorites(user_id):
+    all_user_favorites = Favorite.query.filter_by(user_id = user_id)
+    
+    if all_user_favorites is not None:
+        return jsonify([favorite.serialize() for favorite in all_user_favorites]), 200
+    else:
+        return jsonify({"message": "Favorites not found"}), 404
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/models.py
+++ src/models.py
@@ -1,19 +1,83 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Usuario(db.Model):
+    __tablename__ = 'usuario'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Planeta(db.Model):
+    __tablename__ = 'planeta'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    description = db.Column(db.String(250), nullable=False)
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "description": self.description
+        }
+
+
+class Character(db.Model):
+    __tablename__ = 'character'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    description = db.Column(db.String(250), nullable=False)
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "description": self.description
+        }
+    
+
+class Favorite(db.Model):
+    __tablename__ = 'favorite'
+
+    id = db.Column(db.Integer, primary_key=True)
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'), nullable=True)
+    planeta_id = db.Column(db.Integer, db.ForeignKey('planeta.id'), nullable=True)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
+    character = db.relationship('Character')
+    planeta = db.relationship('Planeta')
+    usuario = db.relationship('Usuario')
+
+    def __init__(self, character_id, planeta_id, usuario_id):
+        self.character_id = character_id
+        self.planeta_id = planeta_id
+        self.usuario_id = usuario_id
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "character": self.character.serialize() if self.character != None else 'No character!',
+            "planeta": self.planeta.serialize() if self.planeta != None else 'No planet!',
+            "usuario": self.usuario.serialize() if self.usuario != None else 'No user!'
         }
